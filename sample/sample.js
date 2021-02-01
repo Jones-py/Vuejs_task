b685b29d5d30413d078b69f2ea4d8346512bd3a2
jones-py <jones03chimfwembe@gmail.com>
2021-02-01 10:47:34 +0000

    first commit

---
+++ sample/sample.js
@@ -0,0 +1,27 @@
+new Vue({
+  // By specifying the HTML id in this el, the instance of Vue.js will be imported in the DOM of that id.
+  el: '#app',
+  // data is an attribute (value) of the defined Vue.js instance
+  data: {
+    name: 'A mountain B ro',
+    course: 'Web Engineer Course',
+    acceptancePeriod: 'January 2019',
+    defaultLastId: 3,
+    students: [
+      { id: 1, name: 'Hiroyoshi Noro', course: 'Machine learning course', acceptancePeriod: 'January 2019' },
+      { id: 2, name: 'Shuji Tominaga', course: 'Web Engineer Course', acceptancePeriod: 'November 2017' },
+      { id: 3, name: 'Kazuki Saito', course: 'Web Engineer Course', acceptancePeriod: 'November 2017' }
+    ]
+  },
+  // In methods, describe the method you want to use for the instance of Vue.js
+  methods: {
+    addStudent: function() {
+      var lastStudentPosition = this.students.length - 1;
+            let id = this.students[lastStudentPosition].id + 1;
+            let name = this.name
+            let course = this.course
+            let acceptancePeriod = this.acceptancePeriod
+            this.students.push({ id,name, course, acceptancePeriod })
+    }
+  }
+})
